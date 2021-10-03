fafadd5fc39f98ed39f926b4f60061384ad4400e
cjh8746 <cjh87467@gmail.com>
2021-10-03 23:31:58 +0900

    feat : RacingCar 생성

---
+++ src/main/java/model/RacingCar.java
@@ -0,0 +1,13 @@
+package model;
+
+public class RacingCar {
+    private final String carName;
+
+    public RacingCar(String value) {
+        this.carName = value;
+    }
+
+    public String getCarName() {
+        return this.carName;
+    }
+}
