297f9e7217366cb4202422d0dbb9ea8945ee6f3e
cjh8746 <cjh87467@gmail.com>
2021-10-03 23:31:38 +0900

    test : 잘못된 위치에 있는 테스트 코드 옮김

--- src/test/java/model/PlayersTest.java
+++ src/test/java/model/PlayersTest.java
@@ -2,8 +2,6 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,18 +9,11 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PlayersTest {
-    @ParameterizedTest
-    @CsvSource(value = {"aron", "king", "sona"})
-    void makeCar(String carName) {
-        RacingCarTest car = new RacingCarTest(carName);
-        assertThat(car.getCarName()).isEquls(carName);
-    }
 
     @Test
     @DisplayName("선수 명단을 만들어 라스트화 하는지 확인")
     void makePlayerRoster() {
-        List<RacingCarTest> playerRoster = new ArrayList<>();
-
+        List<RacingCar> playerRoster = new ArrayList<>();
         assertThat(playerRoster).isEqualTo(updatePlayerRoster());
     }
 
--- src/test/java/model/RacingCarTest.java
+++ src/test/java/model/RacingCarTest.java
@@ -1,36 +1,46 @@
 package model;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class RacingCarTest {
-    private RecordTest record = new RecordTest();
-    private String carName;
-    private RacingCarTest racingCar;
+    private RacingCar racingCar;
 
     @ParameterizedTest
     @CsvSource(value = {"tom,devin,kate,coco,andy"})
     @DisplayName("자동차 이름을 제대로 반환하는지 확인")
-    void getCarName(String value){
-        racingCar = new RacingCarTest(value);
-        assertThat(racingCar.getCarName()).isEqulTo(value);
-       
-    }
-    
-    @Test
-    @DisplayName("자동차의 이름이 5자리 이하인 경우  true를 반환하는지 확인")
-    void verifyNameSize() {
-       assertThat(carName.verifyNameSize()).isTrue();
+    void getCarName(String value) {
+        RacingCar racingCar = new RacingCar(value);
+        assertThat(racingCar.getCarName()).isEqualTo(value);
     }
 
-    @Test
-    @DisplayName("자동차의 이름이 5자리를 초과한 경우 예외처리를 반환하는지 확인"){
-        assertThatThrownBy(() -> carName.verifyNameSize())
+    @ParameterizedTest
+    @CsvSource(value = {"tom,devin,kate,coco,andy"})
+    @DisplayName("자동차의 이름이 5자리 이하인 경우 true를 반환하는지 확인")
+    void verifyNameSize(String value) {
+        racingCar = new RacingCar(value);
+        assertThat(racingCar.verifyNameSize()).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"tomss,devins,katess,cocsso,andyss"})
+    @DisplayName("자동차의 이름이 5자리를 초과한 경우 예외처리를 반환하는지 확인")
+    void verifyNameSize2(String value) {
+        racingCar = new RacingCar(value);
+        assertThatThrownBy(() -> racingCar.verifyNameSize())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"aron", "king", "sona"})
+    void makeCar(String carName) {
+        RacingCar car = new RacingCar(carName);
+        AssertionsForClassTypes.assertThat(car.getCarName()).isEqualTo(carName);
     }
 }
