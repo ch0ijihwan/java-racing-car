4d63563fa5a41ad6f0b9e6ce249710b5360143b6
cjh8746 <cjh87467@gmail.com>
2021-10-03 22:49:34 +0900

    test : RaciongCar 테스크 코드 생성

--- src/test/java/model/RacingCar.java
+++ src/test/java/model/RacingCar.java
@@ -3,14 +3,22 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class RacingCar {
+    private Record record = new Record();
+    private String carName;
+
     @Test
-    @DisplayName("플레이어의 자동차의 이름이 5 이하 인지 확인")
+    @DisplayName("플레이어의 자동차의 이름이 5자리 이하인 경우  true를 반환하는지 확인")
     void verifyNameSize() {
-        boolean result = RacingCar.verifySize();
-        assertThat(result).isTrue();
-        //ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁ
+       assertThat(carName.verifyNameSize()).isTrue();
+    }
+
+    @Test
+    @DisplayName("플레이어의 자동차의 이름이 5자리를 초과한 경우 예외처리를 반환하는지 확인"){
+        assertThatThrownBy(() -> carName.verifyNameSize())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("");
     }
 }
