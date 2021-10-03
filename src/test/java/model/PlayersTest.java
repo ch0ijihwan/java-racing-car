f67ba642e3c79bde3a9f2b1581458509c8933d5d
cjh8746 <cjh87467@gmail.com>
2021-10-04 00:14:16 +0900

    feat : test 코드 수정. 역할 및 책임을 잘못 나누어서 다시 작성하기 위해 지움.

--- src/test/java/model/PlayersTest.java
+++ src/test/java/model/PlayersTest.java
@@ -2,19 +2,41 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PlayersTest {
+    private Players players;
+    private List<RacingCar> playerRoster = new ArrayList<>();
 
     @Test
-    @DisplayName("선수 명단을 만들어 라스트화 하는지 확인")
+    @DisplayName("선수 명단을 만들어 갯수대로 레이싱카를 만드는지 확인")
     void makePlayerRoster() {
-        List<RacingCar> playerRoster = new ArrayList<>();
-        assertThat(playerRoster).isEqualTo(updatePlayerRoster());
+      String [] inputPlayers = {"ass","asd","sdsa","asdf"};
+      assertThat(new Players(inputPlayers).getPlayerRoster().size())
+              .isEqualTo(inputPlayers.length);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"tom,devin,kate,coco,andy"})
+    @DisplayName("자동차의 이름이 5자리 이하인 경우 true를 반환하는지 확인")
+    void verifyNameSize(String value) {
+        assertThat(players.verifyNameSize(value)).isEqualTo(value);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"tomss,devins,katess,cocsso,andyss"})
+    @DisplayName("자동차의 이름이 5자리를 초과한 경우 예외처리를 반환하는지 확인")
+    void verifyNameSize2(String value) {
+        assertThatThrownBy(() -> verifyNameSize(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("");
     }
 
 }
