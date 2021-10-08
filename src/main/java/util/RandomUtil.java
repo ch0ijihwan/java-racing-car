2e8891162efdba4e6b8ebafedc20e29e3abb7937
cjh8746 <cjh87467@gmail.com>
2021-10-09 00:19:24 +0900

    refactor : 디폴트 생성자 프라이빗으로 추가해줌

--- src/main/java/util/RandomUtil.java
+++ src/main/java/util/RandomUtil.java
@@ -3,8 +3,14 @@ package util;
 import java.util.Random;
 
 public class RandomUtil {
+
+    private RandomUtil() {
+
+    }
+
     private static final Random RANDOM = new Random();
-    public static int generateRandomNumber(){
+
+    public static int generateRandomNumber() {
         return RANDOM.nextInt(2);
     }
 }
