841cc93ace3270c38d3a2e02912910ebd741f936
cjh8746 <cjh87467@gmail.com>
2021-10-08 22:13:51 +0900

    refactor : 레이싱의 거리 필드 생성

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -2,9 +2,11 @@ package model;
 
 public class RacingCar {
     private final String carName;
+    private int record;
 
     public RacingCar(String value) {
         this.carName = value;
+        this.record = 0;
     }
 
     public String getCarName() {
