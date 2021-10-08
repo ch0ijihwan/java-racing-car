175265c493da3eaab16fa5afca0b614d2f5f7ea3
cjh8746 <cjh87467@gmail.com>
2021-10-08 22:32:54 +0900

    test : 객체 생성 부분 추가...

--- src/test/java/model/DistanceTest.java
+++ src/test/java/model/DistanceTest.java
@@ -11,6 +11,7 @@ class DistanceTest {
     @Test
     @DisplayName("랜덤으로 거리를 반환하는지 확인. 이때 거리는 0과 1만 반환 한다.")
     void generateRandomDistance() {
+        distance = new Distance();
         assertThat(distance.makeZeroOrOneStep()).isNotNegative().isLessThan(2);
     }
 }
