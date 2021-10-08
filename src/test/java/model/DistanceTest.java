082a2c613ceebca345c77c5a08d36becad58af60
cjh8746 <cjh87467@gmail.com>
2021-10-08 22:14:18 +0900

    test : 한 라운드시 거리를 반환하는 Distance에 대한 테스트 코드 생성

---
+++ src/test/java/model/DistanceTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DistanceTest {
+    @Test
+    @DisplayName("랜덤으로 거리를 반환하는지 확인")
+    void generateRandomDistance() {
+
+    }
+}
