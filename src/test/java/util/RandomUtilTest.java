83695d2bcecbc1df2d1f4d5abe707a70c92d06dc
cjh8746 <cjh87467@gmail.com>
2021-10-09 00:35:36 +0900

    test : RandomUtil에 대한 테스트 코드 구현

--- src/test/java/util/RandomUtilTest.java
+++ src/test/java/util/RandomUtilTest.java
@@ -1,14 +1,37 @@
 package util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static util.RandomUtil.generateRandomNumber;
 
 class RandomUtilTest {
-    @Test
+    @ParameterizedTest
+    @MethodSource("randomUtilParameterProvider")
     @DisplayName("0 혹은 1인 랜덤 수를 반환하는지 확인")
     void generateRandom() {
-        assertThat(RandomUtil.generateRandomNumber()).isNotNegative().isLessThan(2);
+        assertThat(generateRandomNumber()).isNotNegative().isLessThan(2);
+    }
+
+    static Stream<Arguments> randomUtilParameterProvider() {
+        return Stream.of(
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()));
     }
 }
