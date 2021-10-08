b05cf00052a1ef08321f0614371d71761c9d56eb
cjh8746 <cjh87467@gmail.com>
2021-10-08 22:22:21 +0900

    test :  한 스텝 혹은 0스텝을 반환하는지 확인하는 테스트 코드 구현

--- src/test/java/model/DistanceTest.java
+++ src/test/java/model/DistanceTest.java
@@ -3,10 +3,14 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DistanceTest {
-    @Test
-    @DisplayName("랜덤으로 거리를 반환하는지 확인")
-    void generateRandomDistance() {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class DistanceTest {
+    Distance distance;
+
+    @Test
+    @DisplayName("랜덤으로 거리를 반환하는지 확인. 이때 거리는 0과 1만 반환 한다.")
+    void generateRandomDistance() {
+        assertThat(distance.makeZeroOrOneStep()).isNotNegative().isLessThan(2);
     }
 }
