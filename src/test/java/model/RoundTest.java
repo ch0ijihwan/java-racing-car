7adb847ec53cf42e8fd86c6aa56764be8b8ad4a2
cjh8746 <cjh87467@gmail.com>
2021-10-09 17:56:38 +0900

    test : 다수의 승자가 있는 경우를 위해 테스트 코드 수정

--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -9,45 +9,53 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class RoundTest {
+class RoundTest {
     Round round;
 
     @ParameterizedTest
     @MethodSource("racingCarPlayersRosterProvideParameter")
     @DisplayName("RacingCar 리스트에서 승자가 있는 경우 승자의 이름을 반환하는 함수.")
-    void getCheckWinner(List<RacingCar> racingCars, int finishLine ,String expect) {
+    void getCheckWinner(List<RacingCar> racingCars, int finishLine ,String [] expect) {
         round = new Round(racingCars, finishLine);
-        assertThat(round.getNameOfWinner).isEqulsTo(expect);
+        round.checkWinner();
+        assertThat(round.getWinner()).isEqualTo(expect);
     }
 
-    Stream<Arguments> racingCarPlayersRosterProvideParameter() {
+    static Stream<Arguments> racingCarPlayersRosterProvideParameter() {
         return Stream.of(
                 arguments(Arrays.asList(
                         new RacingCar("first", 1),
                         new RacingCar("second", 3),
-                        new RacingCar("third", 10)), 10, "third"),
+                        new RacingCar("third", 10)), 10, new String[]{"third"}),
                 arguments(Arrays.asList(
                         new RacingCar("first", 3),
                         new RacingCar("second", 10),
-                        new RacingCar("third", 2)), 10, "second"),
+                        new RacingCar("third", 2)), 10, new String[]{"second"}),
                 arguments(Arrays.asList(
                         new RacingCar("first", 10),
                         new RacingCar("second", 5),
-                        new RacingCar("third", 2)), 10, "first"),
+                        new RacingCar("third", 2)), 10, new String[]{"first"}),
                 arguments(Arrays.asList(
                         new RacingCar("first", 14),
                         new RacingCar("second", 32),
-                        new RacingCar("third", 100)), 100, "third"),
+                        new RacingCar("third", 100)), 100, new String[]{"third"}),
                 arguments(Arrays.asList(
                         new RacingCar("first", 33),
                         new RacingCar("second", 20),
-                        new RacingCar("third", 100)), 100, "second"),
+                        new RacingCar("third", 100)), 100, new String[]{"second"}),
+                arguments(Arrays.asList(
+                        new RacingCar("first", 100),
+                        new RacingCar("second", 25),
+                        new RacingCar("third", 23)), 100, new String[]{"first"}
+                ),
                 arguments(Arrays.asList(
                         new RacingCar("first", 10),
-                        new RacingCar("second", 25),
-                        new RacingCar("third", 23)), 100, "first")
+                        new RacingCar("second", 10),
+                        new RacingCar("third", 23)), 10, new String[]{"first","second"}
+                )
         );
 
     }
