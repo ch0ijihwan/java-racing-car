1d02b61d2679836e1967dc08a9a687cd34c3acef
cjh8746 <cjh87467@gmail.com>
2021-10-10 22:00:29 +0900

    refcator : 선수명단 객체를 받으면, 받은 선수명단으로 업데이트 하는 생성자 부분 추가

--- src/main/java/model/Players.java
+++ src/main/java/model/Players.java
@@ -2,10 +2,15 @@ package model;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Players {
     private final List<RacingCar> playerRoster;
+
+    public Players(Players playerRoster) {
+        this.playerRoster = playerRoster.playerRoster;
+    }
 
     public Players(String[] inputPlayers) {
 
@@ -15,9 +20,6 @@ public class Players {
         playerRoster = Arrays.stream(inputPlayers)
                 .map(RacingCar::new)
                 .collect(Collectors.toList());
-    }
-    public Players(List<RacingCar> playerRoster) {
-       this.playerRoster = playerRoster;
     }
 
 
@@ -29,5 +31,18 @@ public class Players {
         if (value.length() > 5) {
             throw new IllegalStateException("입력된 이름의 길이가 5보다 깁니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Players players = (Players) o;
+        return Objects.equals(playerRoster, players.playerRoster);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerRoster);
     }
 }
