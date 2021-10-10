937c7504d1ea4768aeee823efbfa58d85c96f115
cjh8746 <cjh87467@gmail.com>
2021-10-10 17:01:41 +0900

    test : 레이싱도중 선수 명단을 불러올 때, 기존의 선수 명단을 반환하는지 확인(간접적으로 반환되는 자동차의 개수가 같은지 확인) 하는 테스트 코드 생성

--- src/test/java/model/RacingTest.java
+++ src/test/java/model/RacingTest.java
@@ -1,57 +1,27 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RacingTest {
-    Racing round;
+    Racing racing;
+    Players players;
 
-//    @Test
-//    @DisplayName("Round객체 생성시 자동차가 움직이는지 확인.")
-//    void checkMove()
-//    {
-//        String[] cars = {"dasf","wqe","qwg","toto"};
-//        Players players = new Players(cars);
-//        round = new Round(players.getPlayerRoster());
-//    }
+    @Test
+    @DisplayName("Round객체 생성시 자동차가 움직이는지 확인.")
+    void checkMove() {
+    }
 
-//    @ParameterizedTest
-//    @MethodSource("racingCarPlayersRosterProvideParameter")
-//    @DisplayName("RacingCar 리스트에서 승자가 있는 경우 승자의 이름을 반환하는 함수.")
-//    void getCheckWinner(List<RacingCar> racingCars, int finishLine ,List<String> expect) {
-//        round = new Round(racingCars);
-//        assertThat(round.getWinner()).isEqualTo(expect);
-//    }
-//
-//    static Stream<Arguments> racingCarPlayersRosterProvideParameter() {
-//        return Stream.of(
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 1),
-//                        new RacingCar("second", 3),
-//                        new RacingCar("third", 10)), 10, Collections.singletonList("third")),
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 3),
-//                        new RacingCar("second", 10),
-//                        new RacingCar("third", 2)), 10, Collections.singletonList("second")),
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 10),
-//                        new RacingCar("second", 5),
-//                        new RacingCar("third", 2)), 10, Collections.singletonList("first")),
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 14),
-//                        new RacingCar("second", 32),
-//                        new RacingCar("third", 100)), 100, Collections.singletonList("third")),
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 33),
-//                        new RacingCar("second", 20),
-//                        new RacingCar("third", 100)), 100, Collections.singletonList("second")),
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 100),
-//                        new RacingCar("second", 25),
-//                        new RacingCar("third", 23)), 100, Collections.singletonList("first")),
-//                arguments(Arrays.asList(
-//                        new RacingCar("first", 10),
-//                        new RacingCar("second", 10),
-//                        new RacingCar("third", 23)), 10, Arrays.asList("first","second")
-//                )
-//        );
-//
-//    }
+    @Test
+    @DisplayName("레이싱도중 선수 명단을 불러올 때, 기존의 선수 명단을 반환하는지 확인(간접적으로 반환되는 자동차의 개수가 같은지 확인)")
+    void getRaicincarRoster() {
+        String[] names = new String[]{"Kim", "Choi", "Yee"};
+        players = new Players(names);
+        racing = new Racing(players.getPlayerRoster());
+
+        assertThat(racing.getRacingcarRouter().size())
+                .isEqualTo(names.length);
+    }
 }
