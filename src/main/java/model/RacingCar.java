c6c4e10eb88dcce8000b74cdcdb383de5d7effc5
cjh8746 <cjh87467@gmail.com>
2021-10-10 22:01:22 +0900

    refactor : 자동차의 이름이 같으면 같은 객체로 인식하도록 @Override equals 함

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -1,6 +1,8 @@
 package model;
 
-public class RacingCar {
+import java.util.Objects;
+
+public class RacingCar {//TODO 두번째 생성자 삭제 고려
     private final String carName;
     private int record;
 
@@ -14,11 +16,28 @@ public class RacingCar {
         this.record = 0;
     }
 
+    public void move(){
+        this.record = this.record + Distance.makeZeroOrOneStep();
+    }
+
     public int getCarRecode(){
-        return record;
+        return this.record;
     }
 
     public String getCarName() {
         return this.carName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RacingCar racingCar = (RacingCar) o;
+        return Objects.equals(carName, racingCar.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName, record);
     }
 }
