01c82565a1096e345a46f8cbbb148b9653305cc2
cjh8746 <cjh87467@gmail.com>
2021-10-11 17:38:17 +0900

    test : 테스트 코드 구현 carName을 반환하는 value를 테스트하는 테스트 코드 구현

--- src/test/java/model/carstatus/CarNameTest.java
+++ src/test/java/model/carstatus/CarNameTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarNameTest {
     private CarName carName;
@@ -28,5 +27,13 @@ public class CarNameTest {
         assertThatThrownBy(() -> new CarName(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름의 길이가 0입니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"tom,tom", "cici,cici","a,a"})
+    @DisplayName("자동차 이름을 반환하는지 확인하는 테스트")
+    void getCarName(String Name, String expect){
+        carName = new CarName(Name);
+        assertThat(carName.value()).isEqualTo(expect);
     }
 }
