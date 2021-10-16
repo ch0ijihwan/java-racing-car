2dda33c28a2c9751c4f65c68bfab09f06aaec3fd
cjh8746 <cjh87467@gmail.com>
2021-10-16 18:01:23 +0900

    test : RacingCar 테스트 코드 구현

--- src/test/java/model/RacingCarTest.java
+++ src/test/java/model/RacingCarTest.java
@@ -1,19 +1,53 @@
 package model;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RacingCarTest {
     private RacingCar racingCar;
 
-    @ParameterizedTest
-    @CsvSource(value = {"tom","devin","kate","coco","andy"})
-    @DisplayName("자동차 이름을 제대로 반환하는지 확인")
-    void getCarName(String value) {
-        RacingCar racingCar = new RacingCar(value);
-        assertThat(racingCar.getCarName()).isEqualTo(value);
+    @Test
+    @DisplayName("객체 생성 시, 이름 하나만 입력 받을 때, 입력 받은 스트링으로 Name객체, 0으로 Distance 객체를 생성하는지 확인한다.")
+    void inputOnlyName() {
+        //given
+        String inputName = "coco";
+
+        //when
+        racingCar = new RacingCar(inputName);
+        String actualCarName = racingCar.getCarName();
+        String expectName = "coco";
+        int actualCarDistance = racingCar.getCarRecode();
+        int expectDistance = 0;
+
+        //then
+        assertAll(
+                ()->assertThat(actualCarName).isEqualTo(expectName),
+                () -> assertThat(actualCarDistance).isEqualTo(expectDistance)
+        );
+    }
+
+    @Test
+    @DisplayName("객체 생성시, 이름과 거리 둘다 입력 받았을 때, 입력 받은 인수로 Disttance 객체를 생성하는지 확인한다.")
+    void inputNameAndDistace()
+    {
+        //given
+        String inputName = "coco";
+        int inputDistance = 3;
+
+        //when
+        racingCar = new RacingCar(inputName, inputDistance);
+        String actualCarName = racingCar.getCarName();
+        String expectName = "coco";
+        int actualCarDistance = racingCar.getCarRecode();
+        int expectDistance = 3;
+
+        //then
+        assertAll(
+                ()->assertThat(actualCarName).isEqualTo(expectName),
+                () -> assertThat(actualCarDistance).isEqualTo(expectDistance)
+        );
     }
 }
