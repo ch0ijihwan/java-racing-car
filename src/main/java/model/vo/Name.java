d246c52847a98bd2d68434857981b025d0f660d0
cjh8746 <cjh87467@gmail.com>
2021-10-16 12:51:47 +0900

    style : 불필요한 줄 띄움 수정

--- src/main/java/model/vo/Name.java
+++ src/main/java/model/vo/Name.java
@@ -1,30 +1,29 @@
-package model.carstatus;
+package model.vo;
 
-public class CarName {
+public class Name {
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int MIN_CAR_NAME_LENGTH = 0;
     String carName;
 
-    public CarName(String carName){
+    public Name(String carName) {
         verifyNameSize(carName);
         isBlank(carName);
         this.carName = carName;
     }
+
     public String value() {
         return carName;
     }
 
     private void verifyNameSize(String value) {
-        if (value.length() > MAX_CAR_NAME_LENGTH ) {
+        if (value.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException("이름의 길이가 5를 초과 했습니다.");
         }
     }
 
-    private void isBlank(String value){
-        if(value.length()==MIN_CAR_NAME_LENGTH){
+    private void isBlank(String value) {
+        if (value.length() == MIN_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException("이름의 길이가 0입니다.");
         }
     }
-
-
 }
