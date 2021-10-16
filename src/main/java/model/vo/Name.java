39bfc306e42048b02aa056b066dfb7ad7328f33b
cjh8746 <cjh87467@gmail.com>
2021-10-16 21:35:37 +0900

    refactor : 이퀄즈 및 해시코드 오버라이드

--- src/main/java/model/vo/Name.java
+++ src/main/java/model/vo/Name.java
@@ -1,5 +1,7 @@
 package model.vo;
 
+import java.util.Objects;
+
 public class Name {
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int MIN_CAR_NAME_LENGTH = 0;
@@ -25,5 +27,18 @@ public class Name {
         if (value.length() == MIN_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException("이름의 길이가 0입니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name = (Name) o;
+        return Objects.equals(carName, name.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName);
     }
 }
