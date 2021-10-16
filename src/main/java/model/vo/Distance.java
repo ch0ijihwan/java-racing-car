ab4f4b3f957d09d064cd29af8616aacb65ced56c
cjh8746 <cjh87467@gmail.com>
2021-10-16 21:34:51 +0900

    refactor : 필드명 변경, 이퀄즈 및 해시코드 오버라이드

--- src/main/java/model/vo/Distance.java
+++ src/main/java/model/vo/Distance.java
@@ -1,18 +1,39 @@
 package model.vo;
 
+import java.util.Objects;
+
 public class Distance {
     private static final int NOT_MOVE = 0;
-    private int movingRecord;
+    private int value;
+    private boolean moving;
 
     public Distance() {
-        this.movingRecord = NOT_MOVE;
+        this.value = NOT_MOVE;
+        moving = false;
+    }
+
+    public Distance(int value) {
+        this.value = value;
     }
 
     public int value() {
-        return movingRecord;
+        return value;
     }
 
     public void update(int movement) {
-        movingRecord = movingRecord + movement;
+        value = value + movement;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance = (Distance) o;
+        return value == distance.value && moving == distance.moving;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, moving);
     }
 }
