0c8711ed0667fb5d06f2dada6bd178c806298004
cjh8746 <cjh87467@gmail.com>
2021-10-17 12:35:58 +0900

    refactor : 랜덤 유틸 삭제 및 랜덤 인터페이스 생성

--- src/main/java/model/movement/RandomMovement.java
+++ src/main/java/model/movement/RandomMovement.java
@@ -3,12 +3,11 @@ package model.movement;
 import java.util.Random;
 
 public class RandomMovement implements MovementStrategy {
-    private static final int TWO_RANDOM_RANGES = 2;
     private static final Random RANDOM = new Random();
 
     @Override
-    public int makeZeroOrOneStep() {
-        return RANDOM.nextInt(TWO_RANDOM_RANGES);
+    public Boolean generateMovement() {
+        return RANDOM.nextBoolean();
     }
 }
 
