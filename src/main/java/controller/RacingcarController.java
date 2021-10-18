0f3c0304a480c99bf03d59c0fd66c6c085f1e430
cjh8746 <cjh87467@gmail.com>
2021-10-18 15:06:09 +0900

    feat : 컨트롤러 생성

---
+++ src/main/java/controller/RacingcarController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.Racing;
+import model.Referee;
+import view.Input;
+import view.StatusDisplay;
+
+public class RacingcarController {
+    private final Racing racing;
+    private int numberOfAttempts;
+
+    public RacingcarController() {
+        StatusDisplay.showInputNams();
+        this.racing = new Racing(Input.inputRacingcarNames());
+        StatusDisplay.showInputNumberOfAttempts();
+        this.numberOfAttempts = Input.inputFnishLine();
+    }
+
+    public void run() {
+        StatusDisplay.showStartGame();
+        Referee referee = new Referee(racing.getRacingCars().getCars());
+        while (numberOfAttempts > 0) {
+            racing.move();
+            referee = new Referee(racing.getRacingCars().getCars());
+            StatusDisplay.showGameStatus(racing.getRacingCars().getCars());
+            numberOfAttempts --;
+        }
+        StatusDisplay.showEndGame(referee.winners(),referee.winners().size());
+    }
+}
