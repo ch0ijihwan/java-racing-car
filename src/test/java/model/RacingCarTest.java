2975cd729c0eb8eb783a71f8366eb10c86e12d96
cjh8746 <cjh87467@gmail.com>
2021-10-18 16:21:09 +0900

    test : move에 대한 테스트 코드 추가

--- src/test/java/model/RacingCarTest.java
+++ src/test/java/model/RacingCarTest.java
@@ -10,59 +10,51 @@ class RacingCarTest {
     private RacingCar racingCar;
 
     @Test
-    @DisplayName("객체 생성 시, 이름 하나만 입력 받을 때, 입력 받은 스트링으로 Name객체, 0으로 Distance 객체를 생성하는지 확인한다.")
-    void inputOnlyName() {
+    @DisplayName("객체 생성시, 입력 받은 인수로 Name과 Disttance 생성하는지 확인한다.")
+    void inputNameAndDistace() {
         //given
         String inputName = "coco";
-
-        //when
-        racingCar = new RacingCar(inputName);
-        String actualCarName = racingCar.getCarName();
         String expectName = "coco";
-        int actualCarDistance = racingCar.getCarRecode();
-        int expectDistance = 0;
-
-        //then
-        assertAll(
-                ()->assertThat(actualCarName).isEqualTo(expectName),
-                () -> assertThat(actualCarDistance).isEqualTo(expectDistance)
-        );
-    }
-
-    @Test
-    @DisplayName("객체 생성시, 이름과 거리 둘다 입력 받았을 때, 입력 받은 인수로 Disttance 객체를 생성하는지 확인한다.")
-    void inputNameAndDistace()
-    {
-        //given
-        String inputName = "coco";
         int inputDistance = 3;
+        int expectDistance = 3;
 
         //when
         racingCar = new RacingCar(inputName, inputDistance);
         String actualCarName = racingCar.getCarName();
-        String expectName = "coco";
         int actualCarDistance = racingCar.getCarRecode();
-        int expectDistance = 3;
 
         //then
         assertAll(
-                ()->assertThat(actualCarName).isEqualTo(expectName),
+                () -> assertThat(actualCarName).isEqualTo(expectName),
                 () -> assertThat(actualCarDistance).isEqualTo(expectDistance)
         );
     }
 
     @Test
     @DisplayName("두 RaicingCar가 가진 Distance(거리)를 비교하여, 값이 같은경우 true을 반환한디.")
-    void equalsCarDistance()
-    {
+    void equalsCarDistance() {
         //given
-        RacingCar racingCar = new RacingCar("a",1);
-        RacingCar anotherRacingcar = new RacingCar("b",1);
+        racingCar = new RacingCar("a", 1);
+        RacingCar anotherRacingcar = new RacingCar("b", 1);
 
         //when
         Boolean actual = racingCar.equalsDistance(anotherRacingcar);
 
         //then
         assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("move 호출시 파라미터로 부터 true을 받으면 distacne의 update를 호출함을 확인한다.")
+    void move() {
+        //given
+        racingCar = new RacingCar("a", 1);
+        int expectDistance = 2;
+
+        //when
+         racingCar.move(true);
+         int actualDistance = racingCar.getCarRecode();
+        //then
+        assertThat(actualDistance).isEqualTo(expectDistance);
     }
 }
