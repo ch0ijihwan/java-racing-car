65d908ca2fab574d13728f1f92d7a4b9c0396493
cjh8746 <cjh87467@gmail.com>
2021-10-18 16:12:09 +0900

    refactor : 불필요한 로직 제거

--- src/main/java/model/vo/Name.java
+++ src/main/java/model/vo/Name.java
@@ -4,24 +4,16 @@ import java.util.Objects;
 
 public class Name {
     private static final int MAX_CAR_NAME_LENGTH = 5;
-    private static final int MIN_CAR_NAME_LENGTH = 0;
     private final String carName;
 
     public Name(String carName) {
         verifyNameSize(carName);
-        isBlank(carName);
         this.carName = carName;
     }
 
     private void verifyNameSize(String value) {
         if (value.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException("이름의 길이가 5를 초과 했습니다.");
-        }
-    }
-
-    private void isBlank(String value) {
-        if (value.length() == MIN_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름의 길이가 0입니다.");
         }
     }
 
--- src/test/java/model/vo/NameTest.java
+++ src/test/java/model/vo/NameTest.java
@@ -1,14 +1,13 @@
 package model.vo;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class NameTest {
-
     @ParameterizedTest
     @DisplayName("자동차의 이름의 길이가 5를 초과하거나 공백을 입력하는 경우 예외처리 반환")
     @CsvSource(value = {"abcdef"})
@@ -17,15 +16,6 @@ class NameTest {
                 .isThrownBy(() -> new Name(value))
                 .withMessage("이름의 길이가 5를 초과 했습니다.");
 
-    }
-
-    @Test
-    @DisplayName("길이가 0인 값이 넘어올 경우 예외처리 반환")
-    void blankTest()
-    {        String value = "";
-        assertThatThrownBy(() -> new Name(value))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름의 길이가 0입니다.");
     }
 
     @ParameterizedTest
