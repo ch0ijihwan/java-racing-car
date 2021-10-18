bca08cc05e15c16f89b12a4b574e4551919fb0ff
cjh8746 <cjh87467@gmail.com>
2021-10-18 14:37:40 +0900

    test : given ,when, Then 구조로 테스트 코드 변경

--- src/test/java/model/RacingcarsTest.java
+++ src/test/java/model/RacingcarsTest.java
@@ -3,14 +3,24 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingcarsTest {
     @Test
     @DisplayName("첫 입력시 선수 명단을 만들어 갯수대로 레이싱카를 만드는지 확인")
     void makePlayerRoster() {
-        String[] inputPlayers = {"ass", "asd", "sdsa", "asdf"};
-        assertThat(new Racingcars(inputPlayers).getCars().size())
-                .isEqualTo(inputPlayers.length);
+        //given
+        String[] inputPlayers = {"ass", "asd", "sdsa"};
+
+        //When
+        Racingcars racingcars = new Racingcars(inputPlayers);
+        List<RacingCar> actual = racingcars.getCars();
+        List<RacingCar> expect = Arrays.asList(new RacingCar("ass"), new RacingCar("asd"), new RacingCar("sdsa"));
+
+        //Then
+        assertThat(actual).isEqualTo(expect);
     }
 }
