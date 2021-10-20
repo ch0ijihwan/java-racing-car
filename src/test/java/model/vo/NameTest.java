d369bfd7eab127f621ba6f0e3b1265b648f940fa
cjh8746 <cjh87467@gmail.com>
2021-10-20 20:43:21 +0900

    test : 가독성을 위해 수정

--- src/test/java/model/vo/NameTest.java
+++ src/test/java/model/vo/NameTest.java
@@ -12,6 +12,7 @@ class NameTest {
     @DisplayName("자동차의 이름의 길이가 5를 초과하거나 공백을 입력하는 경우 예외처리 반환")
     @CsvSource(value = {"abcdef"})
     void getCarName(String value) {
+        //Then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Name(value))
                 .withMessage("이름의 길이가 5를 초과 했습니다.");
@@ -22,7 +23,13 @@ class NameTest {
     @CsvSource(value = {"tom,tom", "cici,cici","a,a"})
     @DisplayName("자동차 이름을 반환하는지 확인하는 테스트")
     void getCarName(String Name, String expect){
+        //given
         Name name = new Name(Name);
-        assertThat(name.value()).isEqualTo(expect);
+
+        //when
+        String actual = name.value();
+
+        //Then
+        assertThat(actual).isEqualTo(expect);
     }
 }
